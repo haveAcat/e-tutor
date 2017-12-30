ea7c5cac2e8489eea9a4ccac6657a09b75c47c93
haveAcat <34856804+haveAcat@users.noreply.github.com>
2017-12-30 16:24:50 +0800

    Update 題目21. 最大值與最小值.cpp
    e-tutor结果显示RE: 無效記憶體引用，将a[9]改为a[10]后完全正确。

--- 題目21. 最大值與最小值.cpp
+++ 題目21. 最大值與最小值.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-	float a[9];
+	float a[10];
 	int ii,jj;
 	float temp;
 	scanf("%f %f %f %f %f %f %f %f %f %f",&a[0],&a[1],&a[2],&a[3],&a[4],&a[5],&a[6],&a[7],&a[8],&a[9]);
